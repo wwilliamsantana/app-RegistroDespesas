f1935da5cbe8145917a3ceacf146239dbb926ddf
William <williamsantana5750@gmail.com>
2022-09-09 16:47:19 -0300

    Validando dados

--- app.js
+++ app.js
@@ -7,30 +7,43 @@ class Despesas {
     this.descricao = descricao
     this.valor = valor
   }
+
+  validarDados() {
+    for (let i in this) {
+      if (this[i] === null || this[i] === undefined || this[i] === '') {
+        return false
+      }
+    }
+    return true
+  }
 }
 
 class Bd {
+  //Ao chamar esta função automaticamente nosso constructor irá funcionar
   constructor() {
-    let id = localStorage.getItem('id')
+    let id = localStorage.getItem('id') //Buscando o valor da chave "id" -> Caso não encontre, vai retornar null
 
     if (id === null) {
+      //-> Caso seja null, vamos atribuir a chave Id, valor 0
       localStorage.setItem('id', 0)
     }
   }
 
   getProximoId() {
-    let proximoId = localStorage.getItem('id')
+    let proximoId = localStorage.getItem('id') //Buscando id novamente
 
-    return parseInt(proximoId) + 1
+    return parseInt(proximoId) + 1 //A cada chamada desta function, iremos incrementar +1
   }
 
   gravarLocal(despesas) {
-    let id = this.getProximoId()
-    localStorage.setItem(id, JSON.stringify(despesas))
+    let id = this.getProximoId() //Pegando o id atual
+    localStorage.setItem(id, JSON.stringify(despesas)) //Adicionando a info no localStorage
 
-    localStorage.setItem('id', id)
+    localStorage.setItem('id', id) //Set do id + novo
   }
 }
+
+let bd = new Bd()
 
 function cadastrarDespesas() {
   let ano = document.getElementById('ano')
@@ -49,7 +62,10 @@ function cadastrarDespesas() {
     valor.value
   )
 
-  let bd = new Bd()
-
-  bd.gravarLocal(despesas)
+  if (despesas.validarDados()) {
+    bd.gravarLocal(despesas)
+    console.log('Dados válidos')
+  } else {
+    console.log('Dados inválidos')
+  }
 }
